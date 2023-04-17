cd8576c4200ebf2880e4d2841968ff6e4277f107
Maks <m.posiel@gmail.com>
2023-04-17 11:12:27 +0200

    further build

--- encoder.c
+++ encoder.c
@@ -11,6 +11,13 @@
 //3 - failed to allocate memory
 //4 - file corrupted
 
+
+// struktura pliku:
+// 1. ilość znaków (int)
+// 2. drzewo
+// 3. zakodoway tekst
+
+
 int encode(char *filename1, char *filename2) {
     if (filename1 == NULL || filename2 == NULL) {
         return 1;
@@ -44,11 +51,31 @@ int encode(char *filename1, char *filename2) {
         cur.freq = node_heap->freq + (node_heap + 1)->freq;
         cur.right = (node_heap + 1);
         cur.left = node_heap;
-        node_heap = delete_first_element(node_heap,&size);
+        cur.is_leaf = 0;
+        node_heap = delete_first_element(node_heap, &size);
         node_heap[0] = cur;
         sort_by_freq(node_heap, size);
     }
 
+    int err1 = write_tree_to_bin(node_heap, out);
+    if (err1 == 1) {
+        return 1;
+    }
+
+
+    int size_of_txt = node_heap->freq;
+
+    struct buffer buf;
+
+    int j = 0;
+    for (int i = 0; i < size_of_txt; ++i) {
+        char *code = code_of_char(node_heap,fgetc(in));
+        if(code==NULL) {
+            return 1;
+        }
+        int code_size = strlen(code);
+        
+    }
 
 
     return 0;
@@ -98,6 +125,7 @@ struct node *char_freq(FILE *f, int *size) {
             (node_tab + j)->data = (freq_tab + i)->data;
             (node_tab + j)->right = NULL;
             (node_tab + j)->left = NULL;
+            (node_tab + j)->is_leaf = 1;
             j++;
         }
     }
@@ -123,12 +151,12 @@ void sort_by_freq(struct node *tab, int size) {
 
 
 struct node *delete_first_element(struct node *tab, int *size) {
-    if(tab==NULL || size==NULL || *size<=0) {
+    if (tab == NULL || size == NULL || *size <= 0) {
         return NULL;
     }
 
-    struct node *new_tab = malloc((*size-1) * sizeof(struct node));
-    if(new_tab==NULL) {
+    struct node *new_tab = malloc((*size - 1) * sizeof(struct node));
+    if (new_tab == NULL) {
         return NULL;
     }
 
@@ -136,6 +164,65 @@ struct node *delete_first_element(struct node *tab, int *size) {
         *(new_tab + i - 1) = *(tab + i);
     }
     free(tab);
-    *size -=1;
+    *size -= 1;
     return new_tab;
 }
+
+int write_tree_to_bin(struct node *n, FILE *f) {
+    if (n == NULL) {
+        return 1;
+    }
+    fwrite(n->is_leaf, sizeof(int), 1, f);
+
+    if (n->is_leaf == 1) {
+        fwrite(n->data, sizeof(char), 1, f);
+    } else {
+        write_tree_to_bin(n->left, f);
+        write_tree_to_bin(n->right, f);
+    }
+    return 0;
+}
+
+char *code_of_char(struct node *root, char ch) {
+    if (root == NULL) {
+        return NULL;
+    }
+
+    if (root->left == NULL && root->right == NULL && root->ch == ch) {
+
+        char *code = (char *) malloc(sizeof(char));
+        code[0] = '\0';
+        return code;
+    }
+
+
+    char *left_code = code_of_char(root->left, ch);
+    if (left_code != NULL) {
+
+        size_t len = strlen(left_code);
+        char *code = (char *) malloc(len + 2);
+        code[0] = '0';
+        strcpy(code + 1, left_code);
+        free(left_code);
+        return code;
+    }
+
+
+    char *right_code = code_of_char(root->right, ch);
+    if (right_code != NULL) {
+        size_t len = strlen(right_code);
+        char *code = (char *) malloc(len + 2);
+        code[0] = '1';
+        strcpy(code + 1, right_code);
+        free(right_code);
+        return code;
+    }
+
+
+    return NULL;
+}
+
+
+
+
+
--- encoder.h
+++ encoder.h
@@ -12,12 +12,26 @@ struct char_frequencies {
 
 
 
-
 struct node {
     char data;
     int freq;
     struct node *left;
     struct node *right;
+    int is_leaf;
+};
+
+union buffer {
+    struct {
+        unsigned char bit0: 1;
+        unsigned char bit1: 1;
+        unsigned char bit2: 1;
+        unsigned char bit3: 1;
+        unsigned char bit4: 1;
+        unsigned char bit5: 1;
+        unsigned char bit6: 1;
+        unsigned char bit7: 1;
+    } bits;
+    unsigned char byte;
 };
 
 int encode(char *filename1, char *filename2);
@@ -28,7 +42,8 @@ void sort_by_freq(struct node *tab, int size);
 
 struct node *delete_first_element(struct node *tab, int *size);
 
+int write_tree_to_bin(struct node *n, FILE *f);
 
-
+char *code_of_char(struct node *root, char ch);
 
 #endif //DATA_COMPRESSOR_ENCODER_H
